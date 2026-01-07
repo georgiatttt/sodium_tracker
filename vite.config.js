63d081e60d7efe16d4cbefb49c1bc9897d14d0a6
Georgia Thomas <georgiathomas@Mac.fios-router.home>
2026-01-07 15:39:11 -0500

    UI and API updates

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,13 @@ export default defineConfig({
   plugins: [react({
     include: '**/*.{js,jsx,ts,tsx,mjs}'
   })],
+  server: {
+    proxy: {
+      '/off': {
+        target: 'https://world.openfoodfacts.org',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/off/, ''),
+      },
+    },
+  },
 })
